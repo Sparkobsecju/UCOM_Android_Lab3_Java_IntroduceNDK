07283d17f67a0829f69db9b6979fca0b8c54cb85
Sivan Chen <ryan1825114@gmail.com>
2024-06-12 16:43:37 +0800

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_lab3_1java_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "使用NDK形成的效果";
+    return env->NewStringUTF(hello.c_str());
+}
---
+++ app/src/main/java/com/example/lab3_java/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab3_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.lab3_java.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'lab3_java' library on application startup.
+    static {
+        System.loadLibrary("lab3_java");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'lab3_java' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
