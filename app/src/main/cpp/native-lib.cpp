e809ec8c6feb29267cc7311441fc8dafd17e7ca4
Sivan Chen <ryan1825114@gmail.com>
2024-06-12 16:59:29 +0800

    add a native function with C plus plus library

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,9 @@ Java_com_example_lab3_1java_MainActivity_stringFromJNI(
     std::string hello = "使用NDK形成的效果";
     return env->NewStringUTF(hello.c_str());
 }
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_lab3_1java_MainActivity_intFromJNI(JNIEnv *env, jobject thiz) {
+    // TODO: implement intFromJNI()
+    return 1314;
+}
